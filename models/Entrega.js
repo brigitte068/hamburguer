a3b3ac6831c6f5df7155e2bd150c24dd1dae86f1
brigitte Lara <brigittelarar59@gmail.com>
2026-04-17 14:37:04 -0300

    correção do model entrega

--- models/Entrega.js
+++ models/Entrega.js
@@ -1,33 +1,36 @@
-import sequelize from "./Database";
-import { DataTypes, Model } from "sequelize";
+import { Model, DataTypes } from "sequelize";
+import dbInstance from "./Database.js";
 
-export default class Entrega extends Model {
-    static associate(models) {
-        Entrega.belongsTo(models.Pedido, {
-            foreignKey: 'pedido_id',
-            as: 'pedido'
-        })
-    }
+class Entrega extends Model {
+  static associate(models) {
+    this.belongsTo(models.Pedido, {
+      foreignKey: 'pedido_id',
+      as: 'pedido'
+    });
+  }
 }
 
 Entrega.init({
-    pedido_id :{
-        type : DataTypes.INTEGER,
-        allowNull : false,
-        primaryKey : true
-    },
-    codigo_rastreio:{
-        type: DataTypes.STRING,
-        allowNull : false,
-        unique : true
-    },
-    endereco :{
-        type: DataTypes.STRING,
-        allowNull : false
-    }
-},sequelize,
-{
-    tableName : 'entregas',
-    timestamps : true, // criar os campos deleteAt e updatedAt
-    paranoid : true
-})
+  pedido_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true
+  },
+  codigo_rastreio: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  endereco: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  sequelize: dbInstance,
+  tableName: 'entregas',
+  modelName: 'Entrega',
+  timestamps: true,
+  paranoid: true
+});
+
+export default Entrega;
